5f2941df707dcd4bcc9537ff5d75ae774bfd203d
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-11 18:53:31 +0500

    Initial Commit

---
+++ ToDoCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoCoreData
+//
+//  Created by Вячеслав Квашнин on 11.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
